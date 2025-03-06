3bb18e0b043ac9a7404bcb24e524c636410cdc91
Suvan77blr <sbumsrit61blr@gmail.com>
2025-03-07 03:05:34 +0530

    POTD_030425_LC : Updated an Innovative Solution
    Checking if a number is a sum of powers of Three. (Prob 1780)
    Added Modulus based approach to solution. => ..._CrazyOptimal();
    Earlier one based on Ternary representation => ..._Intuition()

--- Mar2025/POTD_030425_LC_SumOfPowsOfThree.cpp
+++ Mar2025/POTD_030425_LC_SumOfPowsOfThree.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 class Solution {
     public:
-        bool checkPowersOfThree(int n) {
+        bool checkPowersOfThree_Intuition(int n) {
             // To get the max power of 3 to represent the number 'n'.
             int maxExp = (int)(log10(n) / log10(3)) + 1;
             int sum = 0, val;
@@ -28,6 +28,28 @@ class Solution {
             }
             return false;
         }
+
+        bool checkPowersOfThree_CrazyOptimal(int n) {
+            /*
+                Logic : num%3 can have 3 values : 0,1,2
+                2 - that number can never be expressed as a sum of 3 powers.
+
+                0, 1 - that number may be expressed as a power of 3.
+                    we reupdate the quotient in this case and then continue the iteration.
+            */
+            
+            if(n<0) return false;
+
+            while(n>0) {
+                auto [q, r] = div(n, 3);
+                
+                if(r == 2)
+                    return false;
+
+                n=q;
+            }
+            return true;
+        }
 };
 
 int main(void) {
@@ -36,7 +58,8 @@ int main(void) {
     cin >> n;
 
     Solution soln = Solution();
-    bool result = soln.checkPowersOfThree(n);
+    // bool result = soln.checkPowersOfThree_Intuition(n);
+    bool result = soln.checkPowersOfThree_CrazyOptimal(n);
     cout <<"Is " << n << " a Sum of Powers of 3 ? : " << (result?"True":"False") << endl;
 }
 
